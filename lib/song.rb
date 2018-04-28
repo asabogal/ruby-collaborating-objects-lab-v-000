5562961b94e164fd94b69becdfb831e6e552cf95
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-28 14:45:58 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Song
+
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+  def self.new_by_filename(filename)
+    song_attributes = filename.split(' - ')
+    song = self.new(song_attributes[0])
+    song.artist_name=(song_attributes[1])
+    # song.artist = Artist.find_or_create_by_name(name_artist)
+
+      # binding.pry
+
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+
+  end
+end
