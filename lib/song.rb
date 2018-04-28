93e8802ae121946479956a01cf6b7445a813a829
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-28 15:02:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
 
   def self.new_by_filename(filename)
     song_attributes = filename.split(' - ')
-    song = self.new(song_attributes[0])
+    song = self.new(song_attributes)
     song.artist_name=(song_attributes[1])
     # song.artist = Artist.find_or_create_by_name(name_artist)
 
